1b73bc927ad9a434ed2b000ae2b33c70d48e66a7
Mazel0n <116030053+Mazel0n@users.noreply.github.com>
2023-08-13 20:54:50 +0300

    Add files via upload

---
+++ Vaz 2108/HandbrakeFix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace F100
+{
+    internal class HandbrakeFixF100 : MonoBehaviour
+    {
+        Rigidbody rb, rbvc;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            rbvc = transform.root.GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (rb.mass == 300)
+            {
+                rbvc.Sleep();
+            }
+        }
+    }
+}
